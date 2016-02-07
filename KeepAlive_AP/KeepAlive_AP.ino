dee2a2c44f473ddeaed7464d092a28a078fd56c1
Dad <Dad>
2016-02-07 17:55:52 -0500

    UPDATE

--- KeepAlive_AP/KeepAlive_AP.ino
+++ KeepAlive_AP/KeepAlive_AP.ino
@@ -1,60 +1,17 @@
-//  Description of Project;
-//      To increase the capabilities of Hobby grade RC control. Will be using RC Tanks as test bed.
-
-//  Objective Completed in this Sketch;
-//      1). Controlling Turret and Hull functions without wiring connecting hull and turret. This will
-//          Allow continuous 360 deg. rotations of turret. 
-//          Using Two esp8266's one in the turret and one in the Hull sending Wi-Fi Data from the 
-//          turret to the hull.
-
-//  Objective working on in this Sketch;
-
-//      1). Add Gryo sketch and send by Wi-Fi Gyro Data from the 
-//          turret to the hull.
-
-//      2). Test using the esp8266's i/o controlling RC Tanks functions with the hope of replacing the
-//          Arduino Due mentioned below.
-
-//  Objectives achieved already but not yet added to this project;
-//      1).  Arduino Due connected to RC Rx 8 channel module's servo outputs and have the Arduino Due
-//           control RC tank functions.   
-
-//      2).  Using MPU6050 Gyro connected to Arduino Due to control cannon elevation and 
-//           turret rotation to remain fixed on target as hull is moving.
-
-//      3).  Using the capabilities of FS-TH9X RC radio using ER9X software, transmit 6 switch positions
-//           encoded on one channel and have the Arduino connected to the Rx module decode these
-//           switch position. This gives the ability of 6 full analog and the position of 6 switches.
-//           a total of 13 functions on 8 channels. 
-
-
-
-
-// Notes for this sketch;  
-//    The esp8266 sketch for the turret is referred to as AP and the esp8266 sketch in the hull is STA.
-// 
-//    The time for AP Wi-Fi to Transmit data to STA plus STA to send ACK to AP = approx.0.125ms.
-//    This would cause blocking of both AP and STA sketches of 0.125ms each Wi-Fi transmission this
-//    would make Safe RC control impossible.
-//
-//    Because the Sketch blocking nature of the client.print in include <ESP8266WiFi.h> it is 
-//    not used in AP instead Non Blocking include "pfodESP8266WiFi.h" is used. Also print timeout
-//    in STA is set to zero. This stops the sketch blocking making RC control possible.
-//
-//    AP Wifi_Failue_cnt = number of AP's client.print skipped and STA's client.reads that 
-//    timed out between Wi-Fi actual successful transmissions. Wifi_Failue_cnt in both AP and STA
-//    can be used as a gauge as RC and Gyro programming is added. If or when this count is constantly
-//    zero because of additional programming the frequency of transmission maybe decreasing.
-
 
 //#include <ESP8266WiFi.h>
 #include "pfodESP8266WiFi.h"
-
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "Wire.h"
-
-///////////////////////////////////// GYRO, MPU6050. Declarations ///////////////////////////////////////////////
+/////////////////////////// Declarations Debugging /////////////////////////////////////////////////
+int cnt = 0;
+unsigned long   Time_1=0,Time_1_prev =0,Time_1_max=0;
+float           Time_1_display,Time_1_max_display;
+unsigned long   Time_2=0,Time_2_prev =0,Time_2_max=0;
+float           Time_2_display,Time_2_max_display;
+unsigned long   prev_time =0;
+///////////////////////////////////// GYRO, MPU6050. Declarations //////////////////////////////////
 MPU6050 mpu;
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
@@ -69,39 +26,57 @@ float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gra
 float yaw;
 int Gyro_Status=0;
 
-/////////////////////////// Declarations Debugging /////////////////////////////////////////////////
-int cnt = 0;
-unsigned long   Time_1=0,Time_1_prev =0,Time_1_max=0;
-float           Time_1_display,Time_1_max_display;
-unsigned long   Time_2=0,Time_2_prev =0,Time_2_max=0;
-float           Time_2_display,Time_2_max_display;
-unsigned long   prev_time =0;
-/////////////////////////// Declarations Wifi /////////////////////////////////////////////////////
+///////////////////////////////////// RC. Declarations /////////////////////////////////////////////
+int RC_Loop_Time=0;
+int RC_Status = 0;
+int RC_prev_time =0;
+int elevation_time;
+int switches_time;
+int triger;
+int base;
+int Alloff;
+int TriggerOffSet;
+int Trigger1,Trigger2,Trigger3,Trigger4,Trigger5,Trigger6;
+int Sub1,Sub2,Sub3,Sub4,Sub5,Sub6;
+int Display=0;
+boolean sw1=0,sw2=0,sw3=0,sw4=0,sw5=0,sw6=0;
+
+/////////////////////////// Declarations Wifi //////////////////////////////////////////////////////
 const char* ssid          = "TANK_TURRET_AP";
 const char* password      = "12345679";
 WiFiServer server(80);
 String DataSent;
-int Wifi_Status =0;//......0=OFF, 1=ON, 2=FAILD,3=START .........................................//
-/////////////////////////// Declarations Failure_Detection ////////////////////////////////////////
+int Wifi_Status =0;//......0=OFF, 1=ON, 2=FAILD,3=START ..........................................//
+
+/////////////////////////// Declarations Failure_Detection /////////////////////////////////////////
 int Wifi_Failure_cnt=0;
 int Gyro_Failure_cnt=0;
-int Wiifi_cnt_Trigger = 500;//.............. The cnt in STA and in AP should the same?   ........//
-int Fail_ID;//........... 1=RC, 2=Wifi, 3=Gyro ..................................................//
-//////////////////////////////////////// Failure Dection ROUTINE //////////////////////////////////
+int RC_Failure_cnt=0;
+int RC_cnt_Trigger = 4;//.................. Missed Signals before Safe mod for RC ................//
+int Gyro_cnt_Trigger = 10;//............... fifi overflows before turning gyro off ...............//
+int Wifi_cnt_Trigger = 500;//.............. Missed transmissions before reseting Wyfi ............//
+int Fail_ID;//........... 1=RC, 2=Wifi, 3=Gyro ...................................................//
+
+//////////////////////////////////////// Failure Dection ROUTINE ///////////////////////////////////
 void Failure_Detection(){
-  if(Fail_ID==2){//.................................... ID2=Wifi ................................//
-        Wifi_Failure_cnt = Wifi_Failure_cnt + 1;
-        if(Wifi_Failure_cnt > Wiifi_cnt_Trigger-1){
-            Wifi_Status =2;//......0=OFF, 1=ON, 2=FAILD,3=START .................................//
-            Wifi_Failure_cnt = Wiifi_cnt_Trigger;
+    if(Fail_ID==1){//.................................... ID1=RC .................................//
+      RC_Failure_cnt=RC_Failure_cnt+1;RC_Status = 2;// failed to complete in time for RC Rx ......//
+      if(RC_Failure_cnt > RC_cnt_Trigger){//...................... RC in SAFE mode ...............//
+          RC_Status = 3;RC_Failure_cnt=RC_Failure_cnt-1;}
+    }
+    if(Fail_ID==2){//.................................... ID2=Wifi ...............................//
+        Wifi_Failure_cnt = Wifi_Failure_cnt + 1;//... Not ready for client.print .................//
+        if(Wifi_Failure_cnt > Wifi_cnt_Trigger-1){// Wifi in SAFE mode ...........................//
+            Wifi_Status =2;//......0=OFF, 1=ON, 2=FAILD,3=START ..................................//
+            Wifi_Failure_cnt = Wifi_cnt_Trigger;
             cnt=0;}
-  }
-  if(Fail_ID==31){//................................... ID31=Tried Gyro Start ...................//
-      Gyro_Status=2;}//........................................... Communications failed ........//
-  if(Fail_ID==32){//.......... Gyro ID32; Gyro Data flow ........................................//
-      Gyro_Failure_cnt=Gyro_Failure_cnt+1;Gyro_Status = 3;//.......... Data fifo Reset ..........//
-      if(Gyro_Failure_cnt > 10){//.................................. Eccessive fifi Resets.......//
-          Gyro_Status = 0;Gyro_Failure_cnt=Gyro_Failure_cnt-1;}}  
+    }
+    if(Fail_ID==31){//................................... ID31=Tried Gyro Start ..................//
+        Gyro_Status=2;}//........................................... Communications failed .......//
+    if(Fail_ID==32){//.......... Gyro ID32; Gyro Data flow .......................................//
+        Gyro_Failure_cnt=Gyro_Failure_cnt+1;Gyro_Status = 3;//.......... Data fifo Reset .........//
+        if(Gyro_Failure_cnt >  Gyro_cnt_Trigger){//................... Exccessive fifi Resets.....//
+            Gyro_Status = 0;Gyro_Failure_cnt=Gyro_Failure_cnt-1;}}  
   }
 ///////////////////////////////////// Start GYRO, SubRoutine //////////////////////////////////////
 void startMPU6050(){
@@ -145,6 +120,52 @@ void pgm(){
     if(Gyro_Status==1)getMPU6050data();
     
 }
+
+///////////////////////////////////// RC, SubRoutine ////////////////////////////////////////////////////////////
+void RC(){ 
+    RC_Loop_Time=micros();
+    //..........................................Channel 3, Cannon Elevation ...................................//
+    while(digitalRead(D5)==HIGH){//..................Prevent short timing .....................................//
+      if(micros()-RC_Loop_Time > 20000){Fail_ID=1;Failure_Detection();return;}
+    }
+    while(digitalRead(D5)==LOW){
+      if(micros()-RC_Loop_Time > 20000){Fail_ID=1;Failure_Detection();return;}
+    }
+    RC_prev_time = micros();
+    while(digitalRead(D5)==HIGH);{
+       if(micros()-RC_Loop_Time > 20000){Fail_ID=1;Failure_Detection();return;}
+    }
+    elevation_time = micros()-prev_time;
+    //..........................................Channel 5, 6 x On/Off switches encoded ........................//
+    while(digitalRead(D7)==LOW);{
+      if(micros()-RC_Loop_Time > 20000){Fail_ID=1;Failure_Detection();return;}
+    }
+    RC_prev_time = micros();
+    while(digitalRead(D7)==HIGH);{
+      if(micros()-RC_Loop_Time > 20000){Fail_ID=1;Failure_Detection();return;}
+    }
+    switches_time = micros()-prev_time;Display=switches_time;
+        //................................ Sw. 1 = Wt.16  .....................................................//
+        if(switches_time > Trigger1){switches_time =switches_time-Sub1;sw1=1;}
+        else {sw1=0;}
+        //................................ Sw. 2 = Wt.32  .....................................................//
+        if(switches_time > Trigger2){switches_time = switches_time-Sub2;sw2=1;}
+        else {sw2=0;}
+        //................................ Sw. 3 = Wt.64  .....................................................//
+        if(switches_time > Trigger3){switches_time = switches_time-Sub3;sw3=1;}
+        else {sw3=0;}
+        //................................ Sw. 4 = Wt.128  ....................................................//
+        if(switches_time > Trigger4){ switches_time = switches_time-Sub4;sw4=1;}
+        else {sw4=0;}
+        //................................ Sw. 5 = Wt.128  ....................................................//
+        if(switches_time >Trigger5){switches_time = switches_time-Sub5;sw5=1;}
+        else {sw5=0;}
+        //................................ Sw. 6 = Wt.128  ....................................................//
+        if(switches_time > Trigger6){switches_time = switches_time-Sub6;sw6=1;}
+        else {sw6=0;}
+    RC_Failure_cnt=0;
+    RC_Status = 1;    
+}
 //////////////////////////////////////// Wiifi Start ROUTINE //////////////////////////////////////
 void Wifi_disconnect(){
     WiFi.disconnect();
@@ -161,14 +182,19 @@ void Wifi_AP_start(){
 void Serial_print(){
         Serial.print("Ap Data to Send(");Serial.print(DataSent); Serial.print(") ");
         Serial.print("\t");
+        
         Serial.print("Status; ");
         Serial.print("W(");Serial.print(Wifi_Status);Serial.print(") ");
         Serial.print("G(");Serial.print(Gyro_Status);Serial.print(") ");
+        Serial.print("R(");Serial.print(RC_Status);Serial.print(") ");
         Serial.print("\t");
+        
         Serial.print("Fail_cnts; ");
         Serial.print("W(");Serial.print(Wifi_Failure_cnt);Serial.print(") ");
         Serial.print("G(");Serial.print(Gyro_Failure_cnt);Serial.print(") ");
+        Serial.print("R(");Serial.print(RC_Failure_cnt);Serial.print(") ");
         Serial.print("\t");
+        
         Serial.print("Wifi Transmissions(");Serial.print(cnt);Serial.print("}");
         Serial.println(); 
 }
@@ -191,8 +217,10 @@ void loop() {
     WiFiClient client = server.available();
 
     while(1==1){
-        pgm();//...................................... Simulate Timeing  ............................//
-        
+        //pgm();//...................................... Simulate Timeing  ............................//
+        RC();
+        if(Gyro_Status==0){digitalWrite(16, HIGH);startMPU6050();}
+        if(Gyro_Status==1)getMPU6050data();
         if(Gyro_Status==1)DataSent = ypr[0];
         else {DataSent = "OFF";}
         
--- KeepAlive_STA/KeepAlive_STA.ino
+++ KeepAlive_STA/KeepAlive_STA.ino
@@ -1,5 +1,4 @@
-#include <ESP8266WiFi.h>
-
+#include <ESP8266WiFi.h>       
 /////////////////////////// Declarations Debugging /////////////////////////////////////////////////
 int cnt = 0;
 unsigned long   Time_1=0,Time_1_prev =0,Time_1_max=0;
@@ -7,6 +6,22 @@ float           Time_1_display,Time_1_max_display;
 unsigned long   Time_2=0,Time_2_prev =0,Time_2_max=0;
 float           Time_2_display,Time_2_max_display;
 unsigned long   prev_time =0;
+
+///////////////////////////////////// RC. Declarations /////////////////////////////////////////////
+int RC_Loop_Time=0;
+int RC_Status = 0;
+int RC_prev_time =0;
+int elevation_time;
+int switches_time;
+int triger;
+int base;
+int Alloff;
+int TriggerOffSet;
+int Trigger1,Trigger2,Trigger3,Trigger4,Trigger5,Trigger6;
+int Sub1,Sub2,Sub3,Sub4,Sub5,Sub6;
+int Display=0;
+boolean sw1=0,sw2=0,sw3=0,sw4=0,sw5=0,sw6=0;
+
 /////////////////////////// Declarations Wifi /////////////////////////////////////////////////////
 const char* ssid          = "TANK_TURRET_AP";
 const char* password      = "12345679";
@@ -15,16 +30,80 @@ const int httpPort = 80;
 WiFiClient client;
 String DataRecieved="0";
 int Wifi_Status =0;//......0=OFF, 1=ON, 2=FAILD,3=START .........................................//
-/////////////////////////// Declarations Failure_Detection ////////////////////////////////////////
-int Wiifi_cnt_Trigger = 500;//.............. The cnt in STA and in AP should the same? ..........//
-int Wifi_Failue_cnt=0;
-int Fail_ID;//........... 1=RC, 2=Wifi, 3=Gyro ..................................................//
 
+/////////////////////////// Declarations Failure_Detection ////////////////////////////////////////
+int RC_Failure_cnt=0;
+int Wifi_Failue_cnt=0;
+int RC_cnt_Trigger = 4;//.................. Missed Signals before Safe mod for RC ................//
+int Wifi_cnt_Trigger = 500;//.............. Missed transmissions before reseting Wyfi ............//
+int Fail_ID;//........... 1=RC, 2=Wifi, 3=Gyro ...................................................//
+
+//////////////////////////////////////// Failure Dection ROUTINE ///////////////////////////////////
+void Failure_Detection(){
+    if(Fail_ID==1){//.................................... ID1=RC .................................//
+        RC_Failure_cnt=RC_Failure_cnt+1;RC_Status = 2;// failed to complete in time for RC Rx ....//
+        if(RC_Failure_cnt > RC_cnt_Trigger){//...................... RC in SAFE mode .............//
+            RC_Status = 3;RC_Failure_cnt=RC_Failure_cnt-1;}
+    }
+    if(Fail_ID==2){
+        Wifi_Failue_cnt = Wifi_Failue_cnt + 1;
+        if(Wifi_Failue_cnt > Wifi_cnt_Trigger-1){
+            Wifi_Status =2;//......0=OFF, 1=ON, 2=FAILD,3=START ................................//
+            Wifi_Failue_cnt = Wifi_cnt_Trigger;
+        }
+  }
+  
+}
 //////////////////////////////////////// RC Rx ROUTINE ////////////////////////////////////////////
 void pgm(){
     digitalWrite(BUILTIN_LED, HIGH);//....... RED LED OFF .......................................//
     delay(20);//............................ Simulates RC control routine .......................//
     digitalWrite(BUILTIN_LED, LOW);//....... RED LED ON .........................................//
+}
+///////////////////////////////////// RC, SubRoutine ////////////////////////////////////////////////////////////
+void RC(){ 
+    RC_Loop_Time=micros();
+    //..........................................Channel 3, Cannon Elevation ...................................//
+    while(digitalRead(D5)==HIGH){//..................Prevent short timing .....................................//
+      if(micros()-RC_Loop_Time > 20000){Fail_ID=1;Failure_Detection();return;}
+    }
+    while(digitalRead(D5)==LOW){
+      if(micros()-RC_Loop_Time > 20000){Fail_ID=1;Failure_Detection();return;}
+    }
+    RC_prev_time = micros();
+    while(digitalRead(D5)==HIGH);{
+       if(micros()-RC_Loop_Time > 20000){Fail_ID=1;Failure_Detection();return;}
+    }
+    elevation_time = micros()-prev_time;
+    //..........................................Channel 5, 6 x On/Off switches encoded ........................//
+    while(digitalRead(D7)==LOW);{
+      if(micros()-RC_Loop_Time > 20000){Fail_ID=1;Failure_Detection();return;}
+    }
+    RC_prev_time = micros();
+    while(digitalRead(D7)==HIGH);{
+      if(micros()-RC_Loop_Time > 20000){Fail_ID=1;Failure_Detection();return;}
+    }
+    switches_time = micros()-prev_time;Display=switches_time;
+        //................................ Sw. 1 = Wt.16  .....................................................//
+        if(switches_time > Trigger1){switches_time =switches_time-Sub1;sw1=1;}
+        else {sw1=0;}
+        //................................ Sw. 2 = Wt.32  .....................................................//
+        if(switches_time > Trigger2){switches_time = switches_time-Sub2;sw2=1;}
+        else {sw2=0;}
+        //................................ Sw. 3 = Wt.64  .....................................................//
+        if(switches_time > Trigger3){switches_time = switches_time-Sub3;sw3=1;}
+        else {sw3=0;}
+        //................................ Sw. 4 = Wt.128  ....................................................//
+        if(switches_time > Trigger4){ switches_time = switches_time-Sub4;sw4=1;}
+        else {sw4=0;}
+        //................................ Sw. 5 = Wt.128  ....................................................//
+        if(switches_time >Trigger5){switches_time = switches_time-Sub5;sw5=1;}
+        else {sw5=0;}
+        //................................ Sw. 6 = Wt.128  ....................................................//
+        if(switches_time > Trigger6){switches_time = switches_time-Sub6;sw6=1;}
+        else {sw6=0;}
+    RC_Failure_cnt=0;
+    RC_Status = 1;    
 }
 //////////////////////////////////////// Wiifi Start ROUTINE //////////////////////////////////////
 void Wifi(){
@@ -40,16 +119,7 @@ void Wifi(){
     Wifi_Failue_cnt = 0;
 }
 
-void Failure_Detection(){
-  if(Fail_ID==2){
-        Wifi_Failue_cnt = Wifi_Failue_cnt + 1;
-        if(Wifi_Failue_cnt > Wiifi_cnt_Trigger-1){
-            Wifi_Status =2;//......0=OFF, 1=ON, 2=FAILD,3=START ................................//
-            Wifi_Failue_cnt = Wiifi_cnt_Trigger;
-        }
-  }
-  
-}
+
 void Serial_print(){
     Serial.print("STA Data Rec'd(");
     Serial.print(DataRecieved); Serial.print(") ");
@@ -76,7 +146,7 @@ void loop() {
     Serial.println("Loop restarted");
     Wifi();
     while(1==1){
-        
+        RC();
         Time_1 = micros();
         DataRecieved = client.readStringUntil('\r');//....Timeout is set to ZERO
         if(DataRecieved !=""){
@@ -85,14 +155,14 @@ void loop() {
             Wifi_Failue_cnt = 0;
         }
         else{Fail_ID=2;Failure_Detection();}
-        pgm();//...................................... Delay required for Wifi .................//
+        //pgm();//...................................... Delay required for Wifi .................//
         Serial_print();
         if(Wifi_Status ==2){
             Wifi_Failue_cnt = 0;
             Wifi_Status =3;
             return;
         }
-        pgm();//...................................... Delay required for Wifi .................//
+        //pgm();//...................................... Delay required for Wifi .................//
       }
    
      
